4217a28a644153e65bf952cc33895abbbae17682
Vinicius <vfpv@hotmail.com>
2023-06-24 17:47:42 -0300

    Adicionando arquivos e configurações iniciais

---
+++ src/app/head.tsx
@@ -0,0 +1,7 @@
+export const head = () => {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  )
+}
---
+++ src/components/ListagemVideos.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ListagemVideos = () => {
+  return (
+    <div>ListagemVideos</div>
+  )
+}
